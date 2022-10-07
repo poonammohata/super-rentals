1b67b5c306faa555a0630e02d8932b486540b0e3
Poonam Mohata <poonammohata2@gmail.com>
2022-10-07 19:28:48 +0530

    adds dynamic route

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -10,6 +10,7 @@ module('Integration | Component | rental', function (hooks) {
     // await render(hbs`<Rental />`);
     this.setProperties({
       rental: {
+        id: 'grand-old-mansion',
         title: 'Grand Old Mansion',
         owner: 'Veruca Salt',
         city: 'San Francisco',
@@ -29,6 +30,9 @@ module('Integration | Component | rental', function (hooks) {
     await render(hbs`<Rental @rental={{this.rental}}/>`);
     assert.dom('article').hasClass('rental');
     assert.dom('article h3').hasText('Grand Old Mansion');
+    assert
+      .dom('article h3 a')
+      .hasAttribute('href', '/rentals/grand-old-mansion');
     assert.dom('article .detail.owner').includesText('Veruca Salt');
     assert.dom('article .detail.type').includesText('Standalone');
     assert.dom('article .detail.location').includesText('San Francisco');
